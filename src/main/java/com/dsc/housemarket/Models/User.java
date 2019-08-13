f6a891126f9d3cfba9e757e1acd2b28b69c1b3d9
jeiseel <jeiseel.rodrigues@dce.ufpb.br>
2019-08-12 21:23:27 -0300

    user changes

--- src/main/java/com/dsc/housemarket/Models/User.java
+++ src/main/java/com/dsc/housemarket/Models/User.java
@@ -36,7 +36,6 @@ public class User {
 	@Column
 	private String phone;
 	
-	
 	public long getUser_id() {
 		return user_id;
 	}
@@ -76,4 +75,51 @@ public class User {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		result = prime * result + (int) (user_id ^ (user_id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		if (user_id != other.user_id)
+			return false;
+		return true;
+	}
+
 }
