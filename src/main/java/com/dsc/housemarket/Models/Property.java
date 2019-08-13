b707548ef99cafee858c6e67a0ff4f6d7406e1df
ciacelo <marcelo.martins@dce.ufpb.br>
2019-08-13 19:06:51 -0300

    Add Property class in models

---
+++ src/main/java/com/dsc/housemarket/Models/Property.java
@@ -0,0 +1,96 @@
+package com.dsc.housemarket.Models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Property extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Size(max = 256)
+    @NotEmpty
+    @Column
+    private String name;
+
+    @NotEmpty
+    @Column
+    private float value;
+
+    @Size(max = 256)
+    @NotEmpty
+    @Column
+    private String description;
+
+    @Size(max = 300)
+    @NotEmpty
+    @Column
+    private String superdescription;
+
+    @NotEmpty
+    @Column
+    private Feature features;
+
+    @NotEmpty
+    @Column
+    private Photo photos;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSuperdescription() {
+        return superdescription;
+    }
+
+    public void setSuperdescription(String superdescription) {
+        this.superdescription = superdescription;
+    }
+
+    public Feature getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(Feature features) {
+        this.features = features;
+    }
+
+    public Photo getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(Photo photos) {
+        this.photos = photos;
+    }
+}
