c5a385514dc70c56a4b13451b7f28d649046ab9f
MatheusCrispim <matheus.guedes@dce.ufpb.br>
2019-08-16 15:31:12 -0300

    Feature Model added

---
+++ src/main/java/com/dsc/housemarket/Models/Feature.java
@@ -0,0 +1,60 @@
+package com.dsc.housemarket.Models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Feature extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Size(max = 256)
+    @NotEmpty
+    @Column
+    private String type;
+
+    @Size(max = 256)
+    @NotEmpty
+    @Column
+    private float area;
+
+    @Size(max = 256)
+    @NotEmpty
+    @Column
+    private int rooms;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+    public int getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+}
