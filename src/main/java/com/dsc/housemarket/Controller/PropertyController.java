5654207d28da6e9fbb730aeca57d0c63d21ab24b
Vitor Fernandes <vitor.fernandes.mr@gmail.com>
2019-09-25 02:39:00 -0300

    PropertyController Cors enabled

--- src/main/java/com/dsc/housemarket/Controller/PropertyController.java
+++ src/main/java/com/dsc/housemarket/Controller/PropertyController.java
@@ -11,20 +11,14 @@ import com.dsc.housemarket.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.dsc.housemarket.Models.Property;
 import com.dsc.housemarket.Repository.PropertyRepository;
 
 import static com.dsc.housemarket.Utils.UserUtils.*;
 
+@CrossOrigin("http://localhost")
 @RestController
 @RequestMapping("api")
 public class PropertyController {
